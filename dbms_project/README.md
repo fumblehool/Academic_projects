fd5a0376067fe5fa6c9a92c6e5031b1b5f3646f8
Damanpreet <daman.4880@gmail.com>
2016-05-04 18:07:51 +0530

    added database file

---
+++ dbms_project/README.md
@@ -0,0 +1,8 @@
+# DBMS project
+
+It is a simple demonstration of how a database management system works.
+
+User interface is made using html and php is used to insert and get data from database.
+
+It is a simple Library system which saves records of books in database.
+
---
+++ dbms_project/database/dbms.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2016 at 02:28 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `title` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`title`, `author`) VALUES
+('Concepts Of Physics', 'H.C. Verma'),
+('Harry Potter', 'J.K. Rowling'),
+('Sherlock Holms', 'Sir Arthur Conan Doyle'),
+('Star Trek', 'L.A. Graf');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`title`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
